ac8773216926c3ededbcaa209d080124bf823f5c
Carlos Aviles Buitron <carlosavilesbuitron@carloss-mbp.myfiosgateway.com>
2022-05-11 13:26:10 -0400

    Add react button test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button'
 
 function App() {
 	return (
 		<div className='App'>
 			<h1>Welcome to the app</h1>
+			<Button
+				onClick={() => {
+					console.log('React Click')
+				}}
+			>
+				Log
+			</Button>
 		</div>
 	)
 }
