d9ba74294d685709d485da51280251a7a2d309f0
Carlos Aviles Buitron <carlosavilesbuitron@carloss-mbp.myfiosgateway.com>
2022-05-11 13:20:30 -0400

    Add husky git hooks

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
-import App from './App';
-import './styles/index.scss';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
+import App from './App'
+import './styles/index.scss'
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+const container = document.getElementById('root')
+const root = createRoot(container)
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-);
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>
+)
