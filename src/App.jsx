24c44046ce5bc58026379c9157fa25c6daa04721
Carlos Aviles Buitron <carlosavilesbuitron@Carloss-MacBook-Pro.local>
2022-05-11 15:44:23 -0400

    Add routing and lazy loading of routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,40 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import { Routes, Route, Link, BrowserRouter } from 'react-router-dom'
+
 import Button from 'react-bootstrap/Button'
+
+const Home = React.lazy(() => import('./pages/Home'))
+
+function Loading() {
+	return <p>Loading...</p>
+}
 
 function App() {
 	return (
 		<div className='App'>
-			<h1>Welcome to the app</h1>
-			<Button
-				type='button'
-				onClick={() => {
-					console.log('React Click')
-				}}
-			>
-				Log
-			</Button>
+			<BrowserRouter>
+				<h1>Welcome to the app</h1>
+				<Link to='/'>Home</Link>
+				<br />
+				<Button
+					type='button'
+					onClick={() => {
+						console.log('React Click')
+					}}
+				>
+					Log
+				</Button>
+				<Routes>
+					<Route
+						path='/'
+						element={
+							<Suspense fallback={Loading}>
+								<Home />
+							</Suspense>
+						}
+					/>
+				</Routes>
+			</BrowserRouter>
 		</div>
 	)
 }
